3b8f9e1b55ac5969c4bd8e1f89e95789cd7f3e76
Md-Mobassher <you@example.com>
2022-03-01 13:24:18 +0600

    button api faced

--- js/app.js
+++ js/app.js
@@ -1,34 +1,35 @@
 const allPhones = () => {
     const searchValue = document.getElementById('search-box').value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
-    //console.log(url);
-
+    
     fetch(url)
     .then ((res) => res.json())
     .then ((data) => displayPhones(data.data));
 }
 
 const displayPhones = (phones) =>{
-    console.log(phones);
     for (const phone of phones){
         const phoneContainer = document.getElementById('phones-container');
 
         phones.forEach( phone => {
-            console.log(phone);
+            //console.log(phone);
            const div = document.createElement('div');
+           div.classList = "col-md-4 mx-auto d-flex flex-column justifu-content-center align-items-center";
            div.innerHTML = `
-                <img src="${phone.image}" alt="">
-                <h4>Name:${phone.phone_name}</h4>
-                <h4>Brand:${phone.brand}</h4>
-                <button onclick="" class="bg-primary text-white mx-auto py-2 px-4 border-0 rounded-3 mt-2">Details</button>
+                <img class="img-fluid mb-3" src="${phone.image}" alt="">
+                <h4>Name: ${phone.phone_name}</h4>
+                <h4>Brand: ${phone.brand}</h4>
+                <button onclick="phoneDetails('${phone.slug}')" class="btn-primary text-white mx-auto py-2 px-4 border-0 rounded-3 mt-2">Details</button>
            `;
            phoneContainer.appendChild(div);
         })
-
-        
-
-        console.log(phone);
-
         //console.log(phone);
     }
+}
+
+const phoneDetails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
